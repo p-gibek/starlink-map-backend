07b616732cec05731773908e2c1966d5db69f93e
Piotr Gibek <piotrgibek@outlook.com>
2021-01-31 01:53:38 +0100

    change project structure

---
+++ src/satellites/fetch-satcat-data.ts
@@ -0,0 +1,10 @@
+import { DynamoDB } from 'aws-sdk';
+import { APIGatewayEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
+
+const dynamoDb = new DynamoDB.DocumentClient();
+
+const FetchSATCATData: Handler<APIGatewayEvent, APIGatewayProxyResultV2> = async (event, context) => {
+  return { statusCode: 200, body: JSON.stringify({ x: 'd' }) };
+};
+
+module.exports.FetchSATCATData = FetchSATCATData;
